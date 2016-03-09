4d0c5551a8f2de55b05ef46b566a8f2143477477
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-03-09 19:08:53 +0400

    triangle class

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,33 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define _CRT_NONSTDC_NO_DEPRECATE
 #include <iostream>
-#include "Employee.h"
 using namespace std;
 
+class Triangle {
+public:
+	Triangle() {};
+	~Triangle() {};
+	void setBase(double b) {
+		base = b;
+	}
+	void setHeight(double h) {
+		height = h;
+	}
+private:
+	double base;
+	double height;
+};
+
+Triangle input() {
+	Triangle object;
+	double b, h;
+	cout << "Enter base: ";
+	cin >> b;
+	object.setHeight(b);
+	cout << "Enter height: ";
+	cin >> h;
+	object.setHeight(h);
+	cout << "S = (base * height) / 2 = " << (b * h) / 2 << "\n";
+	return object;
+}
 
 int main() {
 	
-	cout << "Number of employees before instantiation of any objects is "
-		<< Employee::getCount() << endl;
-
-	// the following scope creates and destroys
-	// Employee objects before main terminates
-	{
-		Employee e1("Susan", "Baker");
-		Employee e2("Robert", "Jones");
-
-		cout << "Number of employees after objects are instantiated is "
-			<< Employee::getCount();
-
-		cout << "\n\nEmployee 1: " << e1.getFirstName() << ' ' << e1.getlastName() 
-			<< "\nEmployee 2: " << e2.getFirstName() << ' ' << e2.getlastName() << "\n\n";
-	}
-
-	// no objects exist, so call static member function getCount again
-	// using the class name and the binary scope resolution operator
-	cout << "\nNumber of employees after objects are deleted is "
-		<< Employee::getCount() << "\n";
-
+	Triangle t1 = input();
+	
 	system("pause");
 	return 0;
 }
