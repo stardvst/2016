5310246045627654b3700512ac17a9d15f290d2d
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-04-02 10:29:58 +0400

    Error accessing base class's private data members

---
+++ Project1/Project1/BasePlusCommissionEmployee.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "BasePlusCommissionEmployee.h"
+
+
+
+BasePlusCommissionEmployee::BasePlusCommissionEmployee(
+	const string &first, const string &last, const string &ssn,
+	double sales, double rate, double salary)
+	: CommissionEmployee(first, last, ssn, sales, rate) {
+	setBaseSalary(salary);
+}
+
+void BasePlusCommissionEmployee::setBaseSalary(double salary) {
+	baseSalary = (salary < 0.0) ? 0.0 : salary;
+}
+
+double BasePlusCommissionEmployee::getBaseSalary() const {
+	return baseSalary;
+}
+
+double BasePlusCommissionEmployee::earnings() const {
+	return baseSalary + (commissionRate * grossSales);
+}
+
+void BasePlusCommissionEmployee::print() const {
+	std::cout << "base-salaried commission employee: " << firstName << ' '
+		<< lastName << "\nsocial security number: " << socialSecurityNumber
+		<< "\ngross sales: " << grossSales
+		<< "\ncommission rate: " << commissionRate
+		<< "\nbase salary: " << baseSalary;
+}
