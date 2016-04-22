c97156c829ad0aaf73c06da0f102a7e748382d8c
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-04-22 09:32:06 +0400

    Function Template Overloading

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,47 +1,25 @@
 #include <iostream>
 using namespace std;
 
-template< typename T >
-int printArray(const T * const array, int count, int lowSubscript, int highSubscript) {
-	if (lowSubscript >= 1 && highSubscript <= count && lowSubscript <= highSubscript) {
-		int numberOfElements = 0;
-		for (int i = lowSubscript; i < highSubscript; i++) {
-			cout << array[i] << " ";
-			numberOfElements++;
-		}
-		cout << endl;
-		return numberOfElements;
+template <typename T>
+void printArray(const T *const array, int count) {
+	for (int i = 0; i< count; i++) {
+		cout << array[i] << " ";
 	}
-	return 0;
+	cout << endl;
+}
+
+void printArray(const char *const array, int count) {
+	for (int i = 0; i < count; i++) {
+		cout << array[i] << " ";
+	}
+	cout << endl;
 }
 
 int main() {
 
-	const int aCount = 5; // size of array a
-	const int bCount = 7;
-	const int cCount = 6;
-
-	int a[aCount] = { 1,2,3,4,5 };
-	double b[bCount] = { 1.1,2.2,3.3,4.4,5.5,6.6,7.7 };
-	char c[cCount] = "HELLO";
-
-	cout << "Array a contains" << endl;
-	if (printArray(a, aCount, 2, 4) == 0) {
-		cout << "Subscripts are not correct!\n";
-	}
-	cout << endl;
-
-	cout << "Array b contains" << endl;
-	if (printArray(b, bCount, -5, 4) == 0) {
-		cout << "Subscripts are not correct!\n";
-	}
-	cout << endl;
-
-	cout << "Array c contains" << endl;
-	if (printArray(c, cCount, 5, 4) == 0) {
-		cout << "Subscripts are not correct!\n";
-	}
-	cout << endl;
+	char *arrayOfChar[] = { "Happy", "New", "Year" };
+	printArray(arrayOfChar, 3);
 
 	system("pause");
 	return 0;
