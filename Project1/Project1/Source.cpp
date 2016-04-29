998c226ad0b321a7191a13957e324f49b185167b
Lilia Manukyan <lilia.manukyan@hotmail.com>
2016-04-29 18:48:11 +0400

    Using an iterator to output a string

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -5,30 +5,17 @@
 #include <iomanip>
 using namespace std;
 
-void outputLine(int account, const string name, double balance) {
-	cout << left << setw(10) << account << setw(13) << name
-		<< setw(7) << setprecision(2) << right << balance << endl;
-}
 
 int main() {
 
-	ifstream inClientFile("clients.txt", ios::in);
-	if (!inClientFile) {
-		cerr << "File could not be opened" << endl;
-		exit(1);
+	string str = "Hello";
+	string::const_iterator iter = str.begin();
+	cout << "str = " << str
+		<< "\n(Using iterator iter) str is: ";
+	while (iter != str.end()) {
+		cout << *iter;
+		++iter;
 	}
-
-	int account;
-	string name;
-	double balance;
-
-	cout << left << setw(10) << "Account" << setw(13)
-		<< "Name" << "Balance" << endl << fixed << showpoint;
-
-	while (inClientFile >> account >> name >> balance) {
-		outputLine(account, name, balance);
-	}
-
 
 	cin.get();
 	return 0;
